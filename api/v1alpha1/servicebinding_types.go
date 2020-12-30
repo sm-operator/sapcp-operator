573472e7bc371a55793e3e0c69e87b32881168cd
pavelmaliy <pavel.maliy@sap.com>
2020-12-30 15:51:41 +0200

    add custom columns

--- api/v1alpha1/servicebinding_types.go
+++ api/v1alpha1/servicebinding_types.go
@@ -79,7 +79,9 @@ type ServiceBindingStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-
+// +kubebuilder:printcolumn:JSONPath=".spec.serviceInstanceName",name="Instance",type=string
+// +kubebuilder:printcolumn:JSONPath=".status.conditions[0].reason",name="Status",type=string
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name="Age",type=date
 // ServiceBinding is the Schema for the servicebindings API
 type ServiceBinding struct {
 	metav1.TypeMeta   `json:",inline"`
--- api/v1alpha1/serviceinstance_types.go
+++ api/v1alpha1/serviceinstance_types.go
@@ -78,6 +78,10 @@ type ServiceInstanceStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:JSONPath=".spec.serviceOfferingName",name="Offering",type=string
+// +kubebuilder:printcolumn:JSONPath=".spec.servicePlanName",name="Plan",type=string
+// +kubebuilder:printcolumn:JSONPath=".status.conditions[0].reason",name="Status",type=string
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name="Age",type=date
 
 // ServiceInstance is the Schema for the serviceinstances API
 type ServiceInstance struct {
