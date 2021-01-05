5f166ddd06009a1523cf8fa5fee67052a32f20e5
pavelmaliy <pavel.maliy@sap.com>
2021-01-05 14:51:17 +0200

    more tests

--- controllers/servicebinding_controller_test.go
+++ controllers/servicebinding_controller_test.go
@@ -538,11 +538,6 @@ var _ = Describe("ServiceBinding controller", func() {
 			})
 		})
 
-		XWhen("labels are changed", func() {
-			It("should fail", func() {
-				// TODO labels
-			})
-		})
 	})
 
 	Context("Delete", func() {
