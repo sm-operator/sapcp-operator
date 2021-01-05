5c9a1bb375489810ed58c31353740b3aa99d6f36
kerenlahav <45451976+kerenlahav@users.noreply.github.com>
2021-01-05 12:05:14 +0200

    enable update of binding that is not created yet (failure recovery) (#62)

--- api/v1alpha1/servicebinding_webhook_test.go
+++ api/v1alpha1/servicebinding_webhook_test.go
@@ -31,10 +31,62 @@ var _ = Describe("Service Binding Webhook Test", func() {
 			})
 		})
 
-		Context("Validate update", func() {
+		Context("Validate update of spec before binding is created (failure recovery)", func() {
+			var newBinding *ServiceBinding
+
+			BeforeEach(func() {
+				newBinding = getBinding()
+			})
+			When("Spec changed", func() {
+				When("Service instance name changed", func() {
+					It("should fail", func() {
+						newBinding.Spec.ServiceInstanceName = "new-service-instance"
+						err := newBinding.ValidateUpdate(binding)
+						Expect(err).ToNot(HaveOccurred())
+					})
+				})
+
+				When("External name changed", func() {
+					It("should fail", func() {
+						newBinding.Spec.ExternalName = "new-external-instance"
+						err := newBinding.ValidateUpdate(binding)
+						Expect(err).ToNot(HaveOccurred())
+					})
+				})
+
+				When("Parameters were changed", func() {
+					It("should fail", func() {
+						newBinding.Spec.Parameters = &runtime.RawExtension{
+							Raw: []byte("params"),
+						}
+						err := newBinding.ValidateUpdate(binding)
+						Expect(err).ToNot(HaveOccurred())
+					})
+				})
+			})
+
+			When("Metadata changed", func() {
+				It("should succeed", func() {
+					newBinding.Finalizers = append(newBinding.Finalizers, "newFinalizer")
+					err := newBinding.ValidateUpdate(binding)
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+
+			When("Status changed", func() {
+				It("should succeed", func() {
+					newBinding.Status.BindingID = "12345"
+					err := newBinding.ValidateUpdate(binding)
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+		})
+
+		Context("Validate update of spec after binding is created", func() {
 			var newBinding *ServiceBinding
 			BeforeEach(func() {
 				newBinding = getBinding()
+				newBinding.Status.BindingID = "1234"
 			})
 			When("Spec changed", func() {
 				When("Service instance name changed", func() {
