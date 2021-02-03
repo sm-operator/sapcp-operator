3f1f1a7e541d6edc70aaadb8e8d7c2683eda9e6d
i501080 <keren.lahav@sap.com>
2021-02-03 11:29:58 +0200

    add test

--- controllers/base_controller_test.go
+++ controllers/base_controller_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/sm-operator/sapcp-operator/api/v1alpha1"
 	"github.com/sm-operator/sapcp-operator/internal/secrets"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -17,13 +18,11 @@ var _ = Describe("Base controller", func() {
 	var serviceInstance *v1alpha1.ServiceInstance
 	var fakeInstanceName string
 	var ctx context.Context
-	//var defaultLookupKey types.NamespacedName
 	var controller *BaseReconciler
 
 	BeforeEach(func() {
 		ctx = context.Background()
 		fakeInstanceName = "ic-test-" + uuid.New().String()
-		//defaultLookupKey = types.NamespacedName{Name: fakeInstanceName, Namespace: testNamespace}
 
 		resolver := &secrets.SecretResolver{
 			ManagementNamespace: managementNamespace,
@@ -35,30 +34,53 @@ var _ = Describe("Base controller", func() {
 			Log:            logf.Log.WithName("reconciler"),
 			Client:         k8sClient,
 		}
-
+		serviceInstance = &v1alpha1.ServiceInstance{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "services.cloud.sap.com/v1alpha1",
+				Kind:       "ServiceInstance",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fakeInstanceName,
+				Namespace: testNamespace,
+			},
+			Spec: v1alpha1.ServiceInstanceSpec{
+				ExternalName:        fakeInstanceExternalName,
+				ServicePlanName:     fakePlanName,
+				ServiceOfferingName: fakeOfferingName,
+			},
+		}
 	})
 
 	When("SM secret not exists", func() {
 		It("Should fail with failure condition", func() {
-			serviceInstance = &v1alpha1.ServiceInstance{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "services.cloud.sap.com/v1alpha1",
-					Kind:       "ServiceInstance",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fakeInstanceName,
-					Namespace: testNamespace,
-				},
-				Spec: v1alpha1.ServiceInstanceSpec{
-					ExternalName:        fakeInstanceExternalName,
-					ServicePlanName:     fakePlanName,
-					ServiceOfferingName: fakeOfferingName,
-				},
-			}
 			controller.getSMClient(ctx, controller.Log, serviceInstance)
-
 			Expect(serviceInstance.Status.Conditions[0].Reason).To(Equal(Blocked))
 			Expect(len(serviceInstance.Status.Conditions)).To(Equal(1))
+		})
+	})
+
+	When("SM secret is valid", func() {
+		BeforeEach(func() {
+			namespace := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: managementNamespace}}
+			Expect(k8sClient.Create(context.Background(), namespace)).Should(Succeed())
+			secretWithoutClientID := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      secrets.SAPCPOperatorSecretName,
+					Namespace: managementNamespace,
+				},
+				Data: map[string][]byte{
+					"clientid":     []byte("client-id"),
+					"clientsecret": []byte("client-secret"),
+					"url":          []byte("https://some.url"),
+					"tokenurl":     []byte("https://token.url"),
+				},
+			}
+			Expect(k8sClient.Create(ctx, secretWithoutClientID)).Should(Succeed())
+		})
+		It("Should succeed", func() {
+			client, err := controller.getSMClient(ctx, controller.Log, serviceInstance)
+			Expect(err).To(BeNil())
+			Expect(client).ToNot(BeNil())
 		})
 	})
 })
